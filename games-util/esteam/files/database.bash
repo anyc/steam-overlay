d0e1019b5b64b83e952fbe6a99a174e1be01dc57
Bennjamin Blast <bennjamin.blast@gmail.com>
2017-01-14 19:07:57 -0500

    games-util/esteam: Add Mad Max to UNBUNDLEABLES
    Closes: https://github.com/anyc/steam-overlay/pull/181

--- games-util/esteam/files/database.bash
+++ games-util/esteam/files/database.bash
@@ -1,4 +1,4 @@
-# Copyright 1999-2016 Gentoo Foundation
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 # $Id$
 
@@ -276,6 +276,7 @@ UNBUNDLEABLES=(
 	"Altitude"
 	"Anodyne"
 	"Dwarfs - F2P"
+	"Mad Max"
 	"Revenge of the Titans"
 	"Sid Meier's Civilization V"
 	"Titan Attacks"
