0b4567c67abb53ec393d0082ea9467c53bcc9e1b
James Le Cuirot <chewi@gentoo.org>
2019-04-21 12:36:50 +0100

    games-util/esteam: WormsWMD is unbundleable with lots of new entries
    This game is especially annoying because it bundles FFmpeg 2.8 with
    just about every optional feature enabled, meaning that the likes of
    libbluray are also bundled. We do not have compatibility SLOTs for
    FFmpeg although I will look into it. In the general case, it's also
    hard to tell the difference between libraries that are really used by
    a game and libraries that are there purely to satisfy unused features
    so users will simply have to install these extra packages. Fortunately
    I watch Blu-rays anyway. ;)
    
    Package-Manager: Portage-2.3.64, Repoman-2.3.12
    Signed-off-by: James Le Cuirot <chewi@gentoo.org>

--- games-util/esteam/files/database.bash
+++ games-util/esteam/files/database.bash
@@ -1,12 +1,12 @@
 # Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
-# $Id$
 
 # Please keep these lists sorted!
 
 GCC=\>=sys-devel/gcc-4.6.0
 GLIBC=\>=sys-libs/glibc-2.15
 
+# Commented entries here are deemed to volatile to unbundle.
 LIBS[ld-linux-x86-64.so.2]=${GLIBC}[@MULTILIB@]
 LIBS[ld-linux.so.2]=${GLIBC}[@MULTILIB@]
 LIBS[libacl.so.1]=sys-apps/acl[@ABI@]
@@ -65,6 +65,7 @@ LIBS[libfreetype.so.6]=media-libs/freetype:2[@ABI@]
 LIBS[libgcc_s.so.1]=${GCC}[@MULTILIB@]
 LIBS[libgconf-2.so.4]=gnome-base/gconf:2[@ABI@]
 LIBS[libgcrypt.so.11]=dev-libs/libgcrypt-compat:11[@ABI@]
+LIBS[libgcrypt.so.20]=dev-libs/libgcrypt:0/20[@ABI@]
 LIBS[libgdk-x11-2.0.so.0]=x11-libs/gtk+:2[@ABI@]
 LIBS[libgdk_pixbuf-2.0.so.0]=x11-libs/gdk-pixbuf[@ABI@]
 LIBS[libgdk_pixbuf_xlib-2.0.so.0]=x11-libs/gdk-pixbuf[@ABI@,X]
@@ -74,10 +75,12 @@ LIBS[libGLEW.so.1.6]==media-libs/glew-1.6*[@ABI@]
 LIBS[libglib-2.0.so.0]=dev-libs/glib:2[@ABI@]
 LIBS[libGLU.so.1]=media-libs/glu[@ABI@]
 LIBS[libglut.so.3]=media-libs/freeglut[@ABI@]
+LIBS[libgme.so.0]=media-libs/game-music-emu[@ABI@]
 LIBS[libgmodule-2.0.so.0]=dev-libs/glib:2[@ABI@]
 LIBS[libgmp.so.10]==dev-libs/gmp-6.1*[@ABI@]
 LIBS[libgobject-2.0.so.0]=dev-libs/glib:2[@ABI@]
 LIBS[libgomp.so.1]=${GCC}[@MULTILIB@,openmp]
+LIBS[libgsm.so.1]=media-sound/gsm[@ABI@]
 LIBS[libgpg-error.so.0]=dev-libs/libgpg-error[@ABI@]
 LIBS[libgssapi.so.3]=app-crypt/heimdal[@ABI@]
 LIBS[libgssapi_krb5.so.2]=app-crypt/mit-krb5[@ABI@]
@@ -105,9 +108,11 @@ LIBS[libgtk-x11-2.0.so.0]=x11-libs/gtk+:2[@ABI@]
 LIBS[libgudev-1.0.so.0]=dev-libs/libgudev[@ABI@]
 LIBS[libheimbase.so.1]=app-crypt/heimdal[@ABI@]
 LIBS[libheimntlm.so.0]=app-crypt/heimdal[@ABI@]
+LIBS[libhogweed.so.4]=dev-libs/nettle:0/6.2[@ABI@]
 LIBS[libhx509.so.5]=app-crypt/heimdal[@ABI@]
 LIBS[libICE.so.6]=x11-libs/libICE[@ABI@]
-LIBS[libidn.so.11]=net-dns/libidn[@ABI@]
+LIBS[libidn.so.11]=net-dns/libidn:1.33[@ABI@]
+LIBS[libidn.so.12]=net-dns/libidn:0/12[@ABI@]
 LIBS[libindicator.so.7]=dev-libs/libindicator:0[@ABI@]
 LIBS[libjack.so.0]=media-sound/jack2[@ABI@]
 LIBS[libjacknet.so.0]=media-sound/jack2[@ABI@]
@@ -134,11 +139,14 @@ LIBS[libmenuw.so.6]==sys-libs/ncurses-6*[@ABI@,unicode]
 LIBS[libmikmod.so.2]=media-libs/libmikmod[@ABI@]
 LIBS[libmikmod.so.3]=media-libs/libmikmod[@ABI@]
 LIBS[libMiles.so]=+
+LIBS[libmodplug.so.1]=media-libs/libmodplug[@ABI@]
+LIBS[libmp3lame.so.0]=media-sound/lame[@ABI@]
 LIBS[libmpg123.so.0]=media-sound/mpg123[@ABI@]
 LIBS[libncurses.so.5]==sys-libs/ncurses-5*:5[@ABI@]
 LIBS[libncurses.so.6]==sys-libs/ncurses-6*[@ABI@]
 LIBS[libncursesw.so.5]==sys-libs/ncurses-5*:5[@ABI@,unicode]
 LIBS[libncursesw.so.6]==sys-libs/ncurses-6*[@ABI@,unicode]
+LIBS[libnettle.so.6]=dev-libs/nettle:0/6.2[@ABI@]
 LIBS[libnm-glib.so.4]=net-misc/networkmanager[@ABI@]
 LIBS[libnm-util.so.2]=net-misc/networkmanager[@ABI@]
 LIBS[libnotify.so.4]=x11-libs/libnotify[@ABI@]
@@ -146,9 +154,12 @@ LIBS[libnsl.so.1]=${GLIBC}[@MULTILIB@]
 LIBS[libnspr4.so]=dev-libs/nspr[@ABI@]
 LIBS[libnss3.so]=dev-libs/nss[@ABI@]
 LIBS[libnssutil3.so]=dev-libs/nss[@ABI@]
+LIBS[libnuma.so.1]=sys-process/numactl[@ABI@]
 LIBS[libogg.so.0]=media-libs/libogg[@ABI@]
 LIBS[libopenal.so.1]=media-libs/openal[@ABI@]
+LIBS[libopenjpeg.so.5]=media-libs/openjpeg:0/5[@ABI@]
 LIBS[libopenvr_api.so]=+
+LIBS[libopus.so.0]=media-libs/opus[@ABI@]
 LIBS[liborc-0.4.so.0]=dev-lang/orc[@ABI@]
 LIBS[liborc-test-0.4.so.0]=dev-lang/orc[@ABI@]
 LIBS[libout123.so.0]=media-sound/mpg123[@ABI@]
@@ -178,6 +189,13 @@ LIBS[libpng16.so.16]==media-libs/libpng-1.6*[@ABI@]
 LIBS[libpthread.so.0]=${GLIBC}[@MULTILIB@]
 LIBS[libpulse-simple.so.0]=media-sound/pulseaudio[@ABI@]
 LIBS[libpulse.so.0]=media-sound/pulseaudio[@ABI@]
+#LIBS[libQt5Core.so.5]=dev-qt/qtcore:5[@ABI@]
+#LIBS[libQt5DBus.so.5]=dev-qt/qtdbus:5[@ABI@]
+#LIBS[libQt5Gui.so.5]=dev-qt/qtgui:5[@ABI@]
+#LIBS[libQt5OpenGL.so.5]=dev-qt/qtopengl:5[@ABI@]
+#LIBS[libQt5Widgets.so.5]=dev-qt/qtwidgets:5[@ABI@]
+#LIBS[libQt5X11Extras.so.5]=dev-qt/qtx11extras:5[@ABI@]
+#LIBS[libQt5XcbQpa.so.5]=dev-qt/qtgui:5[@ABI@]
 LIBS[libroken.so.18]=app-crypt/heimdal[@ABI@]
 LIBS[libresolv.so.2]=${GLIBC}[@MULTILIB@]
 LIBS[librt.so.1]=${GLIBC}[@MULTILIB@]
@@ -198,7 +216,9 @@ LIBS[libSDL_ttf-2.0.so.0]=media-libs/sdl-ttf[@ABI@]
 LIBS[libselinux.so.1]=sys-libs/libselinux[@ABI@]
 LIBS[libSM.so.6]=x11-libs/libSM[@ABI@]
 LIBS[libsmime3.so]=dev-libs/nss[@ABI@]
+LIBS[libsnappy.so.1]=app-arch/snappy:0/1[@ABI@]
 LIBS[libsndfile.so.1]=media-libs/libsndfile[@ABI@]
+LIBS[libsoxr.so.0]=media-libs/soxr[@ABI@]
 LIBS[libspeex.so.1]=media-libs/speex[@ABI@]
 LIBS[libspeexdsp.so.1]=media-libs/speex[@ABI@]
 LIBS[libsqlite3.so.0]=dev-db/sqlite:3[@ABI@]
@@ -210,9 +230,12 @@ LIBS[libstdc++.so.5]=sys-libs/libstdc++-v3[@MULTILIB@]
 LIBS[libstdc++.so.6]=${GCC}[@MULTILIB@,cxx]
 LIBS[libsteam_api.so]=+
 LIBS[libSteamValidateUserIDTickets_amd64.so]=+
+LIBS[libtasn1.so.6]=dev-libs/libtasn1:0/6[@ABI@]
 LIBS[libtbb.so.2]=dev-cpp/tbb[@ABI@]
 LIBS[libtbbmalloc.so.2]=dev-cpp/tbb[@ABI@]
 LIBS[libtbbmalloc_proxy.so.2]=dev-cpp/tbb[@ABI@]
+#LIBS[libtcmalloc.so.4]==dev-util/google-perftools-2*[@ABI@]
+#LIBS[libtcmalloc_minimal.so.4]==dev-util/google-perftools-2*[@ABI@]
 LIBS[libtdb.so.1]=sys-libs/tdb[@ABI@]
 LIBS[libtheora.so.0]=media-libs/libtheora[@ABI@]
 LIBS[libtheoradec.so.1]=media-libs/libtheora[@ABI@]
@@ -230,6 +253,7 @@ LIBS[libtiffxx.so.5]=media-libs/tiff:0[@ABI@,cxx]
 LIBS[libtinfo.so.5]==sys-libs/ncurses-5*:5[@ABI@,tinfo]
 LIBS[libtinfo.so.6]==sys-libs/ncurses-6*[@ABI@,tinfo]
 LIBS[libtogl.so]=+
+LIBS[libtwolame.so.0]=media-sound/twolame[@ABI@]
 LIBS[libtxc_dxtn.so]=media-libs/libtxc_dxtn[@ABI@]
 LIBS[libudev.so.0]=sys-libs/libudev-compat[@ABI@]
 LIBS[libusb-1.0.so.0]=dev-libs/libusb:1[@ABI@]
@@ -244,6 +268,7 @@ LIBS[libvorbisfile.so.3]=media-libs/libvorbis[@ABI@]
 LIBS[libvstdlib.so]=+
 LIBS[libvstdlib_s.so]=+
 LIBS[libvstdlib_srv.so]=+
+LIBS[libwavpack.so.1]=media-sound/wavpack[@ABI@]
 LIBS[libwayland-client.so.0]=dev-libs/wayland[@ABI@]
 LIBS[libwayland-cursor.so.0]=dev-libs/wayland[@ABI@]
 LIBS[libwayland-egl.so.1]=media-libs/mesa[@ABI@,wayland]
@@ -274,8 +299,10 @@ LIBS[libxslt.so.1]=dev-libs/libxslt[@ABI@]
 LIBS[libXss.so.1]=x11-libs/libXScrnSaver[@ABI@]
 LIBS[libXt.so.6]=x11-libs/libXt[@ABI@]
 LIBS[libXtst.so.6]=x11-libs/libXtst[@ABI@]
+LIBS[libxvidcore.so.4]=media-libs/xvid[@ABI@]
 LIBS[libXxf86vm.so.1]=x11-libs/libXxf86vm[@ABI@]
 LIBS[libz.so.1]=sys-libs/zlib[@ABI@]
+LIBS[libzvbi.so.0]=media-libs/zvbi[@ABI@]
 LIBS[vgui.so]=+
 
 UNBUNDLEABLES=(
@@ -305,6 +332,7 @@ UNBUNDLEABLES=(
 	"Titan Attacks"
 	"Tomb Raider"
 	"Trine 2"
+	"WormsWMD"
 )
 
 # The following lack unbundleable libraries:
