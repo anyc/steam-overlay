cbe3bc682f8b39df304b9a6d4055473b7720653a
James Le Cuirot <chewi@gentoo.org>
2021-08-04 23:09:02 +0100

    games-util/steam-launcher: Hack to make LD_LIBRARY_PATH work again
    Sorry this took so long.
    
    Package-Manager: Portage-3.0.20, Repoman-3.0.3
    Signed-off-by: James Le Cuirot <chewi@gentoo.org>

--- games-util/steam-launcher/files/steam-wrapper.sh
+++ games-util/steam-launcher/files/steam-wrapper.sh
@@ -19,4 +19,9 @@ unset IFS
 # Add paths to occasionally needed libraries not found in /usr/lib.
 export LD_LIBRARY_PATH+="${LD_LIBRARY_PATH+:}@@GENTOO_LD_LIBRARY_PATH@@"
 
+# Steam renames LD_LIBRARY_PATH to SYSTEM_LD_LIBRARY_PATH and it then becomes
+# ineffective against games. We unfortunately therefore have to force the value
+# through via STEAM_RUNTIME_LIBRARY_PATH instead.
+export STEAM_RUNTIME_LIBRARY_PATH="${LD_LIBRARY_PATH}"
+
 . "${0%/*}"/../lib/steam/bin_steam.sh
