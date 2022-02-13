f2c2eaf447183fd98ea6e6abe11b75590aae5866
James Le Cuirot <chewi@gentoo.org>
2022-02-13 23:09:16 +0000

    games-util/steam-launcher: Remove old SDL controller workaround
    While this was useful once, it seems to be doing more harm than good now. Steam
    and games under it were seeing duplicate devices, and sometimes even duplicate
    button presses.
    
    Signed-off-by: James Le Cuirot <chewi@gentoo.org>

--- games-util/steam-launcher/files/steam-wrapper.sh
+++ games-util/steam-launcher/files/steam-wrapper.sh
@@ -7,15 +7,6 @@ export STEAM_RUNTIME
 # Gentoo's lsb-release doesn't set this.
 export DISTRIB_RELEASE="@@PVR@@"
 
-# Find joystick devices to make Steam's old SDL library use them.
-IFS=$'\n'
-for f in $(find /dev/input -maxdepth 1 -type c | sort --version-sort); do
-	if udevadm info --query=property --name="$f" 2>/dev/null | grep --quiet ID_INPUT_JOYSTICK=1; then
-		export SDL_JOYSTICK_DEVICE+=${SDL_JOYSTICK_DEVICE+:}$f
-	fi
-done
-unset IFS
-
 # Add paths to occasionally needed libraries not found in /usr/lib.
 export LD_LIBRARY_PATH+="${LD_LIBRARY_PATH+:}@@GENTOO_LD_LIBRARY_PATH@@"
 
