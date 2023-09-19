13debbd88a91e89a27b1b5b312dffad7ed112dd7
Andrew Ammerlaan <andrewammerlaan@gentoo.org>
2023-09-19 16:56:21 +0200

    games-util/steam-launcher: Fix desktop layout for controllers in wayland
    Upstream issue https://github.com/ValveSoftware/steam-for-linux/issues/4738
    
    From what I understand Valve points to wayland, and wayland points to valve,
    in the meantime this has been broken for way too long with no fix in sight.
    
    The problem originates in the lack of the X11 XTEST extension in wayland
    sessions.
    
    Solution is:
    - extest: this always works
    - xwayland[libei] with a compositor that also supports libei (currently only
    >=x11-wm/mutter-45.0 )
    
    Since extest always works this is the first choice. If the library is present
    the wrapper LD_PRELOAD's it as per upstream instructions.
    
    Signed-off-by: Andrew Ammerlaan <andrewammerlaan@gentoo.org>

--- games-util/steam-launcher/files/steam-wrapper.sh
+++ games-util/steam-launcher/files/steam-wrapper.sh
@@ -10,6 +10,12 @@ export DISTRIB_RELEASE="@@PVR@@"
 # Add paths to occasionally needed libraries not found in /usr/lib.
 export LD_LIBRARY_PATH+="${LD_LIBRARY_PATH+:}@@GENTOO_LD_LIBRARY_PATH@@"
 
+# Preload the extest library when running in wayland session
+if [[ -f "@@GENTOO_X86_LIBDIR@@/libextest.so" &&
+		${XDG_SESSION_TYPE} == wayland ]]; then
+	export LD_PRELOAD+="${LD_PRELOAD+:}@@GENTOO_X86_LIBDIR@@/libextest.so"
+fi
+
 # Steam renames LD_LIBRARY_PATH to SYSTEM_LD_LIBRARY_PATH and it then becomes
 # ineffective against games. We unfortunately therefore have to force the value
 # through via STEAM_RUNTIME_LIBRARY_PATH instead.
