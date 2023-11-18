9f702b31ca4cc4f3b41b34ebf18fcf1b2c095231
James Le Cuirot <chewi@gentoo.org>
2023-11-18 23:53:31 +0000

    games-util/steam-launcher: Preload 32-bit libstdc++ on default-libcxx systems
    This avoids crashes.
    
    Closes: https://github.com/anyc/steam-overlay/pull/342
    Signed-off-by: James Le Cuirot <chewi@gentoo.org>

--- games-util/steam-launcher/files/steam-wrapper.sh
+++ games-util/steam-launcher/files/steam-wrapper.sh
@@ -10,10 +10,20 @@ export DISTRIB_RELEASE="@@PVR@@"
 # Add paths to occasionally needed libraries not found in /usr/lib.
 export LD_LIBRARY_PATH+="${LD_LIBRARY_PATH+:}@@GENTOO_LD_LIBRARY_PATH@@"
 
-# Preload the extest library when running in wayland session
-if [[ -f "@@GENTOO_X86_LIBDIR@@/libextest.so" &&
-		${XDG_SESSION_TYPE} == wayland ]]; then
+# Preload the extest library when running in a Wayland session.
+[[ -f "@@GENTOO_X86_LIBDIR@@/libextest.so" && ${XDG_SESSION_TYPE} == wayland ]] &&
 	export LD_PRELOAD+="${LD_PRELOAD+:}@@GENTOO_X86_LIBDIR@@/libextest.so"
+
+# Preload libstdc++ on default-libcxx systems to avoid crashes. Loading the
+# 64-bit library prevents Steam from working at all, so only load the 32-bit
+# multlib library. Pure 32-bit systems are untested and may not work.
+if grep -Fxqe --stdlib=libc++ /etc/clang/gentoo-runtimes.cfg 2>/dev/null; then
+	IFS=:
+	for GCC_LIB_DIR in $(gcc-config -L 2>/dev/null); do
+		[[ ${GCC_LIB_DIR} == */32 ]] || continue
+		export LD_PRELOAD+="${LD_PRELOAD+:}${GCC_LIB_DIR}/libstdc++.so"
+	done
+	unset IFS GCC_LIB_DIR
 fi
 
 # Steam renames LD_LIBRARY_PATH to SYSTEM_LD_LIBRARY_PATH and it then becomes
