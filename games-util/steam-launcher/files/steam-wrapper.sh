a825572a0858e9e5a84723b4f60405ecd38a5be1
ThisNekoGuy <28468662+ThisNekoGuy@users.noreply.github.com>
2023-11-18 11:08:58 +0000

    Simplified the libcxx Segfault Fix

--- games-util/steam-launcher/files/steam-wrapper.sh
+++ games-util/steam-launcher/files/steam-wrapper.sh
@@ -10,16 +10,15 @@ export DISTRIB_RELEASE="@@PVR@@"
 # Add paths to occasionally needed libraries not found in /usr/lib.
 export LD_LIBRARY_PATH+="${LD_LIBRARY_PATH+:}@@GENTOO_LD_LIBRARY_PATH@@"
 
-# Preload the extest library when running in wayland session
-if [ -f `find "/usr/lib/gcc/x86_64-pc-linux-gnu/$(gcc --version | grep gcc | sed -E 's|[A-Za-z]+\s+\([^)]*\)\s+||' | cut -f 1 -d '.')/32" -type l -name "libstdc++.so"` ] &&
-		[ "$(eselect profile list | grep '\*' | grep clang)" != "" ] &&
-  		[ -f "@@GENTOO_X86_LIBDIR@@/libextest.so" ] &&
-  		[ "$XDG_SESSION_TYPE" = "wayland" ]; then
-	export LD_PRELOAD+="${LD_PRELOAD+:}@@GENTOO_X86_LIBDIR@@/libextest.so"
+
+if [ "$(eselect profile list | grep '\*' | grep clang)" != "" ] || [ "$(eselect profile list | grep '\*' | grep llvm)" != "" ]; then
 	# Fixes Steam not properly launching on LLVM+Wayland systems
 	# Backticks (``) are being used because it captures spaces inside paths; not that there should be. Shellcheck will complain about this.
-	export LD_PRELOAD+="${LD_PRELOAD+:}`find "/usr/lib/gcc/x86_64-pc-linux-gnu/$(gcc --version | grep gcc | sed -E 's|[A-Za-z]+\s+\([^)]*\)\s+||' | cut -f 1 -d '.')/32" -type l -name "libstdc++.so"`"
-elif [[ -f "@@GENTOO_X86_LIBDIR@@/libextest.so" &&
+	export LD_PRELOAD+="${LD_PRELOAD+:}`find /usr/lib/gcc/x86_64-pc-linux-gnu/$(gcc --version | grep gcc | sed -E 's|[A-Za-z]+\s+\([^)]*\)\s+||' | cut -f 1 -d '.')/32 -type l -name 'libstdc++.so'`"
+fi
+
+# Preload the extest library when running in wayland session
+if [[ -f "@@GENTOO_X86_LIBDIR@@/libextest.so" &&
 		${XDG_SESSION_TYPE} == wayland ]]; then
 	export LD_PRELOAD+="${LD_PRELOAD+:}@@GENTOO_X86_LIBDIR@@/libextest.so"
 fi
